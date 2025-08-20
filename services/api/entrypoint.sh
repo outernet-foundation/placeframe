55b2f6fedfba8eb8e2eebab845b07a00024dc4a2
Tyler Hatch <tyler@tylerhatch.com>
2025-08-20 12:19:39 -0400

    Move --no-sync to the right place

--- services/api/entrypoint.sh
+++ services/api/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd /app/services/api 
-uv run uvicorn src.main:app --no-sync --host 0.0.0.0 --port 8000
+uv run --no-sync uvicorn src.main:app --host 0.0.0.0 --port 8000
