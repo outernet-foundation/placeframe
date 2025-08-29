257921a0f25231c796ab8ab1cb7df6bbc7fc0a45
Tyler Hatch <tylershatch@gmail.com>
2025-08-29 16:53:12 -0400

    wip

--- common/src/common/batch_client_aws.py
+++ common/src/common/batch_client_aws.py
@@ -1,8 +1,16 @@
-from typing import Dict
+from typing import TYPE_CHECKING, Any, Dict
+
+import boto3
+
+if TYPE_CHECKING:
+    from mypy_boto3_batch import BatchClient as AwsBatchClient
+else:
+    BatchClient = Any
 
 
 class BatchClient:
-    def __init__(self): ...
+    def __init__(self):
+        self.client: AwsBatchClient = boto3.client("batch", region_name="us-east-1")  # type: ignore[call-arg]
 
     def submit_job_array(
         self,
@@ -12,7 +20,20 @@ class BatchClient:
         array_size: int,
         environment_variables: Dict[str, str],
     ):
-        return 0
+        response = self.client.submit_job(
+            jobName=name,
+            jobQueue=queue_name,
+            jobDefinition=job_definition_name,
+            arrayProperties={"size": array_size},
+            containerOverrides={
+                "environment": [
+                    {"name": key, "value": value}
+                    for key, value in environment_variables.items()
+                ]
+            },
+        )
+
+        return response["jobId"]
 
     def get_job_status(self, job_id: str):
-        return "SUCCEEDED"
+        return self.client.describe_jobs(jobs=[job_id])["jobs"][0]["status"]
--- common/src/common/batch_client_compose.py
+++ common/src/common/batch_client_compose.py
@@ -1,16 +1,8 @@
-from typing import TYPE_CHECKING, Any, Dict
-
-import boto3
-
-if TYPE_CHECKING:
-    from mypy_boto3_batch import BatchClient as AwsBatchClient
-else:
-    BatchClient = Any
+from typing import Dict
 
 
 class BatchClient:
-    def __init__(self):
-        self.client: AwsBatchClient = boto3.client("batch", region_name="us-east-1")  # type: ignore[call-arg]
+    def __init__(self): ...
 
     def submit_job_array(
         self,
@@ -20,20 +12,7 @@ class BatchClient:
         array_size: int,
         environment_variables: Dict[str, str],
     ):
-        response = self.client.submit_job(
-            jobName=name,
-            jobQueue=queue_name,
-            jobDefinition=job_definition_name,
-            arrayProperties={"size": array_size},
-            containerOverrides={
-                "environment": [
-                    {"name": key, "value": value}
-                    for key, value in environment_variables.items()
-                ]
-            },
-        )
-
-        return response["jobId"]
+        return 0
 
     def get_job_status(self, job_id: str):
-        return self.client.describe_jobs(jobs=[job_id])["jobs"][0]["status"]
+        return "SUCCEEDED"
