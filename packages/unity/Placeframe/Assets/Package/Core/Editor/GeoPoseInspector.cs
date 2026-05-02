5efc10baf97a2f05619f6c94d7490a458cd6b310
Tyler Hatch <tylershatch@gmail.com>
2026-05-02 15:28:24 -0400

    Rename Anchor to GeoPose, drop its per-frame Lerp, add SE(3) Interpolate

--- packages/unity/Placeframe/Assets/Package/Core/Editor/GeoPoseInspector.cs
+++ packages/unity/Placeframe/Assets/Package/Core/Editor/GeoPoseInspector.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace Placeframe.Core
 {
-    [CustomEditor(typeof(Anchor))]
-    public class AnchorInspector : Editor
+    [CustomEditor(typeof(GeoPose))]
+    public class GeoPoseInspector : Editor
     {
         public override void OnInspectorGUI()
         {
-            var target = (Anchor)this.target;
+            var target = (GeoPose)this.target;
 
             EditorGUILayout.LabelField("ECEF");
             EditorGUI.indentLevel++;
