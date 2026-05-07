f5c6554c8c25595f9123d65324c2ee2005136da7
Tyler Hatch <tylershatch@gmail.com>
2026-05-07 16:10:12 -0400

    Merge AppSetup into App

--- apps/AndroidMobile/Assets/Scripts/Capture/App.cs
+++ apps/AndroidMobile/Assets/Scripts/Capture/App.cs
@@ -2,13 +2,23 @@ using FofX.Stateful;
 using FofX;
 using Nessle;
 using ObserveThing;
+using Outernet.Logging;
+using Placeframe.Core;
 using UnityEngine;
 using static Placeframe.Client.UIElements;
+#if !UNITY_EDITOR
+using Placeframe.Core.ARFoundation;
+#endif
 
 namespace Placeframe.Client
 {
     public class App : AppBase<AppState>
     {
+        public SceneReferences sceneReferences;
+        public UIPrimitiveSet uiPrimitives;
+        public UIElementSet uiElements;
+        public LocalizationMapManager localizationMapManager;
+
         private IControl ui;
 
         protected override void InitializeState(AppState state)
@@ -19,8 +29,31 @@ namespace Placeframe.Client
 
         protected override void Awake()
         {
+            Logger<LogGroup>.Initialize();
+            sceneReferences.Initialize();
+
+            Application.targetFrameRate = 120;
+            UIBuilder.primitives = uiPrimitives;
+            UIElements.elements = uiElements;
+
             base.Awake();
             ui = AppUI();
+
+            App.state.placeframeAuthAudience.value = "placeframe-api";
+
+            Instantiate(localizationMapManager);
+
+#if UNITY_EDITOR
+            var cameraProvider = new NoOpCameraProvider();
+#else
+            var cameraProvider = new CameraProvider(SceneReferences.ARCameraManager, SceneReferences.ARAnchorManager);
+#endif
+
+            CaptureManager.Initialize(cameraProvider);
+            LocalizationManager.Initialize(cameraProvider);
+            AuthManager.Initialize();
+            SettingsManager.Initialize();
+            CaptureController.Initialize();
         }
 
         void OnDestroy()
